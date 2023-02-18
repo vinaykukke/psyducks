7e8fa1f676ca106fc47ef9e097db663174bf26b1
vinay kukke <kukke06@gmail.com>
2023-02-19 02:01:18 +0530

    Updating the hh tasks for the new contracts

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,49 +1,67 @@
-import { task } from "hardhat/config";
+import { task, types } from "hardhat/config";
 import { getWallet, getLocalWallet } from "./helpers/wallet";
 
-task("deploy", "Deploy NFT contract").setAction(async (_taskArgs, hre) => {
-  const timestamp = Math.round(Date.now() / 1000);
+task("deploy", "Deploy NFT contract")
+  .addParam("name", "The name of the contract to be deployed", "", types.string)
+  .setAction(async (taskArgs: { name: string }, hre) => {
+    if (!taskArgs.name) {
+      console.error(
+        "Contract name not specified. Please provide a name using the --name flag."
+      );
+      return;
+    }
 
-  const [deployer] = await hre.ethers.getSigners();
-  const accountBalance = await deployer.getBalance();
-
-  const contract = await hre.ethers.getContractFactory("PsyDucks", getWallet());
-  /** If there is an argument in the constructor of the contract pass it here */
-  const psyDucks = await contract.deploy();
-
-  /** Deploying the contract to the network */
-  await psyDucks.deployed();
-
-  console.table({
-    "Contract Owner": deployer.address,
-    "Account Balance": accountBalance.toString(),
-    "Contract Address": psyDucks.address,
-    "Time Stamp": timestamp,
-  });
-});
-
-task("deploy-local", "Deploy NFT contract to local network").setAction(
-  async (_taskArgs, hre) => {
     const timestamp = Math.round(Date.now() / 1000);
-
     const [deployer] = await hre.ethers.getSigners();
     const accountBalance = await deployer.getBalance();
 
     const contract = await hre.ethers.getContractFactory(
-      "PsyDucks",
-      getLocalWallet()
+      taskArgs.name,
+      getWallet()
     );
     /** If there is an argument in the constructor of the contract pass it here */
-    const psyDucks = await contract.deploy();
+    const psy = await contract.deploy();
 
     /** Deploying the contract to the network */
-    await psyDucks.deployed();
+    await psy.deployed();
 
     console.table({
       "Contract Owner": deployer.address,
       "Account Balance": accountBalance.toString(),
-      "Contract Address": psyDucks.address,
+      "Contract Address": psy.address,
       "Time Stamp": timestamp,
     });
-  }
-);
+  });
+
+task("deploy-local", "Deploy NFT contract to local network")
+  .addParam("name", "The name of the contract to be deployed", "", types.string)
+  .setAction(async (taskArgs: { name: string }, hre) => {
+    if (!taskArgs.name) {
+      console.error(
+        "Contract name not specified. Please provide a name using the --name flag."
+      );
+      return;
+    }
+
+    const timestamp = Math.round(Date.now() / 1000);
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
+
+    const contract = await hre.ethers.getContractFactory(
+      taskArgs.name,
+      getLocalWallet()
+    );
+
+    /** If there is an argument in the constructor of the contract pass it here */
+    const psy = await contract.deploy();
+
+    /** Deploying the contract to the network */
+    await psy.deployed();
+
+    console.table({
+      "Contract Owner": deployer.address,
+      "Account Balance": accountBalance.toString(),
+      "Contract Address": psy.address,
+      "Time Stamp": timestamp,
+    });
+  });
